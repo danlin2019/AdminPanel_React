67a767f0019b653274afe83903594d9041a84715
danlin <asmob2011@gmail.com>
2025-01-14 23:45:25 +0800

    Updates

---
+++ functions/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
+    "no-unused-vars": ["warn"],
+    "no-undef": "off", // 關閉未定義變數的檢查
+  },
+};
